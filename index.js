7d9a2c5c5bc15378e82c4cc1951af36bd1df6536
Robert Plummer <robertleeplummerjr@gmail.com>
2018-06-18 12:37:43 -0400

    update brain, still breaking, but later in compilation

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var net = new brain.FeedForward({
   inputLayer: () => input({height: 28, width: 28}),
   hiddenLayers: [
-    input => convolution({width: 5, height: 28, filters: 8, stride: 1, padding: 2, filterHeight: 24, filterWidth: 24, filterCount: 8}, input),
+    input => convolution({filters: 8, stride: 1, padding: 2, filterHeight: 5, filterWidth: 5, filterCount: 8}, input),
     input => relu(input),
     input => pool({width: 2, stride: 2}, input),
-    input => convolution({width: 5, filters: 16, stride: 1, padding: 2, filterHeight: 12, filterWidth: 12, filterCount: 16}, input),
+    input => convolution({filters: 16, stride: 1, padding: 2, filterHeight: 5, filterWidth: 5, filterCount: 16}, input),
     input => relu(input),
     input => pool({width: 3, stride: 3}, input),
     input => softMax({width: 10}, input)
   ],
-  outputLayer: input => output({width: 10}, input)
+  outputLayer: input => target({width: 10, height: 10}, input)
 });
 
 loadJSON('data/training.json')
@@ -17,7 +17,7 @@ loadJSON('data/training.json')
     net.train(trainingData);
   });
 
-loadJSON('data/test.json')
-  .then(function(testData) {
-    document.body.appendChild(toImage(randomImageData(testData)));
-  });
+// loadJSON('data/test.json')
+//   .then(function(testData) {
+//     document.body.appendChild(toImage(randomImageData(testData)));
+//   });
