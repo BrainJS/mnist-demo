a0b4f2879595d9073629d286ab15dc8fb528b07d
Robert Plummer <robertleeplummerjr@gmail.com>
2018-07-17 11:24:55 -0400

    adding fullyConnected layer in, was a missing link

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var net = new brain.FeedForward({
   hiddenLayers: [
     input => convolution({filters: 8, stride: 1, padding: 2, filterHeight: 5, filterWidth: 5, filterCount: 8}, input),
     input => relu(input),
-    input => pool({width: 2, stride: 2}, input),
+    input => pool({filters: 8, filterWidth: 2, filterHeight: 2, filterCount: 8, stride: 2}, input),
     input => convolution({filters: 16, stride: 1, padding: 2, filterHeight: 5, filterWidth: 5, filterCount: 16}, input),
     input => relu(input),
-    input => pool({width: 3, stride: 3}, input),
+    input => pool({filterWidth: 3, filterHeight: 3, filterCount: 16, stride: 3}, input),
+    input => fullyConnected({ depth: 10 }, input),
     input => softMax({width: 10}, input)
   ],
-  outputLayer: input => target({width: 10, height: 10}, input)
+  outputLayer: input => target({width: 1, height: 10}, input)
 });
 
 loadJSON('data/training.json')
